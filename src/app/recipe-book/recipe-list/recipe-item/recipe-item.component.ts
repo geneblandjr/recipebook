9cc34c3deecfe3dda5ac6c4c0d9fdbed6541d606
Gene <geneblandjr@gmail.com>
2017-05-20 14:02:18 -0400

    Save to switch projects.  in process of implementing recipe detail from model.  Looking for ngfor logic...

--- src/app/recipe-book/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipe-book/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+      new Recipe('A Test Recipe', "This a test recipe for example only, and probably won't taste very good.", 'http://maxpixel.freegreatpicture.com/static/photo/2x/Cooking-Delicious-Korean-Food-Recipe-Asia-822090.jpg' )
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
